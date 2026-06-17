04c0eb4b1e85d83c3ef5b67fefbc5eb20e845ec1
Pamela Iwabuchi <199025978+pamelaiwabuchi@users.noreply.github.com>
2026-06-16 23:44:23 -0300

    Refactor code structure for improved readability and maintainability

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 USE universidade_unitev;
     CREATE TABLE unitev (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(255),
-        email VARCHAR(255),
-        sugestao TEXT
-    );
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    sugestao TEXT
+);
